73721827047b7788cb3e7d14092f4d1a167ac141
Maiorana Juan Diego <juanmaiorana23@gmail.com>
2023-05-28 12:15:33 -0300

    PreEntrega1

--- data.js
+++ data.js
@@ -1,32 +1,94 @@
 let comando = 0;
+let carrito = 0;
+let remeras = 0;
+let pantalones = 0;
+let accesorios = 0;
 
-while (comando != "Salir"); {
-    comando = prompt("1) Enviar mensaje:\n 2) Salir \n 3)Hack")
+while (comando != "Salir") {
+
+    comando = parseInt(prompt("1) Productos\n 2) Metodos de pago \n 3)Carrito \n 4) Salir"));
 
     switch (comando) {
+
+        // PRODUCTOS
         case 1:
-            let destinatario = prompt("Ingrese el destinatario");
-            let mensaje = prompt("Ingrese su ensaje:");
-            alert("Mensaje " + mensaje + "enviado a" + destinatario);
-            break;
+            let list2 = parseInt(prompt("Agregar producto al carrito: \n 1)Remeras \n 2)Pantalones \n 3)Accesorios"));
 
+            if (list2 == 1) {
+                let eleccion = parseInt(prompt("Desea agregar 'Remeras' al carrito? \n 1)Si \n 2)No"));
+                if (eleccion == 1) {
+                    alert("Producto agregado al carrito....");
+                    carrito = carrito + 1;
+                    remeras = remeras + 1;
+                    continue;
+                } else if (eleccion == 2) {
+                    alert("Volviendo al principio....");
+                    continue;
+                } else {
+                    alert("error");
+                    continue;
+                }
+            } else if (list2 == 2) {
+                let eleccion = parseInt(prompt("Desea agregar 'Pantalones' al carrito? \n 1)Si \n 2)No"));
+                if (eleccion == 1) {
+                    alert("Producto agregado al carrito....");
+                    carrito = carrito + 1;
+                    pantalones = pantalones + 1;
+                    continue;
+                } else if (eleccion == 2) {
+                    alert("Volviendo al principio....");
+                    continue;
+                } else {
+                    alert("error");
+                    continue;
+                }
+            } else if (list2 == 3) {
+                let eleccion = parseInt(prompt("Desea agregar 'Accesoios' al carrito? \n 1)Si \n 2)No"));
+                if (eleccion == 1) {
+                    alert("Producto agregado al carrito....");
+                    carrito = carrito + 1;
+                    accesorios = accesorios + 1;
+                    continue;
+                } else if (eleccion == 2) {
+                    alert("Volviendo al principio....");
+                    continue;
+                } else {
+                    alert("error");
+                    continue;
+                }
+            }
+
+        // METODOS DE PAGO
         case 2:
-            for (let i = 1; i <= 3; i++) {
-                alert("producto N°" + i);
+            let metodos = prompt("Elija el metodo de pago:\n 1)Credito \n 2)Debito \n 3)Efectivo");
+            if (metodos == 1) {
+                alert("Con tarjetas de credito tenes hasta 12 cuotas sin interes.")
+            } else if (metodos == 2) {
+                alert("Con tarjetas de debito tenes hasta 3 cuotas en los productos de indumentaria.");
+            } else if (metodos == 3) {
+                alert("Las compras en efectivo son de pago unico y en mano.");
+            } else {
+                alert("ERROR");
             }
             break;
 
+        // CARRITO
         case 3:
-            alert("Conectandose a la IP de la NASA... Aguarde un momento por favor.");
-            let clave = promp("Ingrese la clave por favor:");
-            if (clave == 1234) {
-                alert("Conectado con la base de datos de la NASA. Obteniendo informacion EXTRATERRESTRE")
-                window.location = "";
-                comando = "Salir";
-                break;
+            if (carrito == 0) {
+                alert("Sin productos en el carrito");
+            } else {
+                alert("Productos agregados al carrito: \n" + remeras + " Remeras \n" + pantalones + " Pantalones \n" + accesorios + " Accesorios");
             }
+            break;
+
+        // SALIR
+        case 4:
+            comando = "Salir";
+            break;
+
         default:
             alert("este comando no existe");
             break;
+
     }
 }
