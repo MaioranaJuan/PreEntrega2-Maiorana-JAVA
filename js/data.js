fea3c5e401791b23154fde7236eb0864da8ab7de
Maiorana Juan Diego <juanmaiorana23@gmail.com>
2023-07-05 13:27:58 -0300

    3ra Pre Entrega

--- js/data.js
+++ js/data.js
@@ -1,120 +1,152 @@
 class Ropa {
-    constructor(nombre, precio, foto) {
+    constructor(nombre, precio, foto, id) {
         this.nombre = nombre;
         this.precio = precio;
         this.foto = foto;
+        this.id = id;
     }
 }
 
 let Carrito = [];
 
-// variables
+// Variables
 let pago = 0;
-let cancelar = 0;
-let ProductosEnCarrito = "";
 let finalizado = false;
 
 // Productos
-const bandanaAkatzuki = new Ropa("Bandana Akatzuki", 1300, "Bandana-Konoha.png");
-const bodyAkatzuki = new Ropa("Body Akatzuki", 2000, "BodyAkatzuki.webp");
-const buzoUchiha = new Ropa("Buzo Uchiha", 5000, "Buzo-Uchiha.webp");
-const BuzoUchihaRojo = new Ropa("Buzo Uchiha Rojo", 5000, "BuzoUchihaRojo.webp");
-const PantuflasAkatzuki = new Ropa("Pantuflas Akatzuki", 1500, "PantuflasAkatzuki.webp");
-const ConjuntoNaruto = new Ropa("Conjunto Naruto", 2300, "ConjuntoNaruto.webp");
-const GorraAkatzuki = new Ropa("Gorra Akatzuki", 1800, "GorraAkatzuki.webp");
-const MediasNaruto = new Ropa("Medias Naruto", 500, "MediasNaruto.webp");
-const BufandaAkatzuki = new Ropa("Bufanda Akatzuki", 800, "Bufanda Akatzuki.webp");
+const bandanaAkatzuki = new Ropa("Bandana Akatzuki", 1300, "Bandana-Konoha.png", 1);
+const bodyAkatzuki = new Ropa("Body Akatzuki", 2000, "BodyAkatzuki.webp", 2);
+const buzoUchiha = new Ropa("Buzo Uchiha", 5000, "Buzo-Uchiha.webp", 3);
+const BuzoUchihaRojo = new Ropa("Buzo Uchiha Rojo", 5000, "BuzoUchihaRojo.webp", 4);
+const PantuflasAkatzuki = new Ropa("Pantuflas Akatzuki", 1500, "PantuflasAkatzuki.webp", 5);
+const ConjuntoNaruto = new Ropa("Conjunto Naruto", 2300, "ConjuntoNaruto.webp", 6);
+const GorraAkatzuki = new Ropa("Gorra Akatzuki", 1800, "GorraAkatzuki.webp", 7);
+const MediasNaruto = new Ropa("Medias Naruto", 500, "MediasNaruto.webp", 8);
+const BufandaAkatzuki = new Ropa("Bufanda Akatzuki", 800, "Bufanda Akatzuki.webp", 9);
+
 
 const elementoPago = document.querySelector("#Precio");
 const elementoCarrito = document.querySelector("#Carrito");
-const HabilitarCompra = document.getElementById('Compra');
 const HabilitarLimpieza = document.getElementById('Limpieza');
+const btnCompra = document.getElementById('btnCompra');
 
-// precio del carrito
+const carritoDataJSON = localStorage.getItem('carrito');
+if (carritoDataJSON) {
+    const carritoData = JSON.parse(carritoDataJSON);
+    Carrito = carritoData.carrito.map(producto => new Ropa(producto.nombre, producto.precio, producto.foto));
+    pago = carritoData.pago;
+
+    Carrito.forEach(ropa => {
+        let elementoRopa = `
+        <li>
+            <img src="assets/${ropa.foto}" class="imgCarrito"/>
+            <p class="imgCarrito"><span class="carritoText">${ropa.nombre}</span> $${ropa.precio}</p>
+        </li>
+        `;
+        elementoCarrito.innerHTML += elementoRopa;
+    });
+}
+
 elementoPago.innerText = pago;
 
-// agregado de productos al carrito
-function comprar(Ropa) {
-    Carrito.push(Ropa);
-    pago = pago + Ropa.precio;
+HabilitarLimpieza.disabled = Carrito.length === 0;
+btnCompra.disabled = Carrito.length === 0;
+
+// Agregar producto al carrito
+function comprar(ropa) {
+    Carrito.push(ropa);
+    pago += ropa.precio;
     let elementoRopa = `
     <li>
-        <img src="assets/${Ropa.foto}" class="imgCarrito"/>
-        <p class="imgCarrito"><span class="carritoText">${Ropa.nombre}</span> $${Ropa.precio}</p>
+        <img src="assets/${ropa.foto}" class="imgCarrito"/>
+        <p class="imgCarrito"><span class="carritoText">${ropa.nombre}</span> $${ropa.precio}</p>
     </li>
     `;
     elementoCarrito.innerHTML += elementoRopa;
     elementoPago.innerText = pago;
-
-    if (Carrito.length > 0) {
-        HabilitarCompra.disabled = false;
-        HabilitarLimpieza.disabled = false;
-    }
-    ProductosEnCarrito = Carrito.toString();
+    HabilitarLimpieza.disabled = false;
+    btnCompra.disabled = false;
+    guardarCarrito();
 }
+const botonLimpiar = document.getElementById('Limpieza');
 
-function Asegurar() {
-    let CompraFinal = parseInt(prompt("Desea utilizar este método de pago?\n 1) Sí\n 2) No"));
+botonLimpiar.addEventListener('click', function () {
+    Limpiar();
+});
 
-    if (CompraFinal === 1) {
-        alert("Gracias por confiar en nosotros a la hora de hacer su compra");
-        alert("Usted ha comprado " + ProductosEnCarrito);
-        Carrito.splice(0, Carrito.length);
-        Carrito = [];
-        elementoCarrito.innerHTML = "";
-        pago = 0;
-        elementoPago.innerText = pago;
-        HabilitarCompra.disabled = true;
-        HabilitarLimpieza.disabled = true;
-        finalizado = true;
-    } else if (CompraFinal === 2) {
-        alert("Redirigiendo....");
-    }
-}
-
-
-function Finalizar() {
-    while (cancelar != 3 && !finalizado) {
-        let direccion = prompt("Ingrese su dirección para continuar con la compra:");
-
-        let confirmar = parseInt(prompt("Su dirección es '" + direccion + "'.\n¿Desea cambiar la dirección?\n 1) Sí\n 2) No\n 3) Cancelar Operación"));
-
-        switch (confirmar) {
-            case 1:
-                alert("Redirigiendo...");
-                continue;
-
-            case 2:
-                let metodos = prompt("Elija el método de pago:\n 1) Crédito\n 2) Débito/Efectivo");
-
-                if (metodos == 1) {
-                    alert("Con tarjetas de crédito tienes hasta 12 cuotas sin interés.");
-                    alert("Precio Final: $" + pago + " en 12 cuotas de $" + pago / 12);
-                    Asegurar();
-                } else if (metodos == 2) {
-                    alert("Con efectivo el pago es de $" + pago);
-                    Asegurar();
-                } else {
-                    alert("ERROR");
-                }
-                continue;
-
-            case 3:
-                cancelar = 3;
-                finalizado = true;
-                break;
-        }
-    }
-}
-
+// Limpiar carrito
 function Limpiar() {
-    let limpieza = parseInt(prompt("Esta seguro que desea limpiar el carrito? \n 1)Si \n 2)No"));
-    if (limpieza == 1) {
-        Carrito = [];
-        elementoCarrito.innerHTML = "";
-        pago = 0;
-        elementoPago.innerText = pago;
-        HabilitarCompra.disabled = true;
-        HabilitarLimpieza.disabled = true;
-    }
+    Carrito = [];
+    elementoCarrito.innerHTML = "";
+    pago = 0;
+    elementoPago.innerText = pago;
+    HabilitarLimpieza.disabled = true;
+    btnCompra.disabled = true;
+    guardarCarrito();
 }
+
+function guardarCarrito() {
+    const carritoData = {
+        carrito: Carrito.map(ropa => ({ nombre: ropa.nombre, precio: ropa.precio, foto: ropa.foto })),
+        pago: pago
+    };
+    const carritoDataJSON = JSON.stringify(carritoData);
+    localStorage.setItem('carrito', carritoDataJSON);
+}
+
+const botonBandana = document.getElementById('comprarBandana');
+const botonBody = document.getElementById('comprarBody');
+const botonBuzo = document.getElementById('comprarBuzo');
+const botonBuzoRojo = document.getElementById('comprarBuzoRojo');
+const botonPantuflas = document.getElementById('comprarPantuflas');
+const botonConjunto = document.getElementById('comprarConjunto');
+const botonGorra = document.getElementById('comprarGorra');
+const botonMedias = document.getElementById('comprarMedias');
+const botonBufanda = document.getElementById('comprarBufanda');
+
+botonBandana.addEventListener('click', function () {
+    comprar(bandanaAkatzuki);
+});
+
+botonBody.addEventListener('click', function () {
+    comprar(bodyAkatzuki);
+});
+
+botonBuzo.addEventListener('click', function () {
+    comprar(buzoUchiha);
+});
+
+botonBuzoRojo.addEventListener('click', function () {
+    comprar(BuzoUchihaRojo);
+});
+
+botonPantuflas.addEventListener('click', function () {
+    comprar(PantuflasAkatzuki);
+});
+
+botonConjunto.addEventListener('click', function () {
+    comprar(ConjuntoNaruto);
+});
+
+botonGorra.addEventListener('click', function () {
+    comprar(GorraAkatzuki);
+});
+
+botonMedias.addEventListener('click', function () {
+    comprar(MediasNaruto);
+});
+
+botonBufanda.addEventListener('click', function () {
+    comprar(BufandaAkatzuki);
+});
+
+btnCompra.addEventListener('click', function () {
+    Swal.fire({
+        title: 'Gracias por su compra',
+        icon: 'success',
+        text: 'Pedido en camino...',
+        showConfirmButton: false,
+        timer: 2000
+    })
+    Limpiar();
+});
